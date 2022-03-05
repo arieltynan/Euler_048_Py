955b498410ccb46e2825a2f0ac4fa745783a317c
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-05 00:33:03 -0500

    comments

--- Euler_048_Py.py
+++ Euler_048_Py.py
@@ -15,34 +15,30 @@ def powerLL(x, n): # Function derived from "mits" on GeeksforGeeks
     result = 1;
     while (n):
         if (n & 1):
-            result = result * x % MOD;
-        n = int(n / 2);
-        x = x * x % MOD;
-    return result;
+            result = result * x % MOD
+        n = int(n / 2)
+        x = x * x % MOD
+    return result
  
 def powerStrings(sa, sb): # Function derived from "mits" on GeeksforGeeks
      
     # We convert strings to number
-    a = 0;
-    b = 0;
+    a = b = 0
  
     # calculating a % MOD
-    for i in range(len(sa)):
-        a = (a * 10 + (ord(sa[i]) -
-                       ord('0'))) % MOD;
+    for i in range(len(sa)): #Shuffles through each digit, multipling by 10, adding next digit, taking modulus
+        a = (a * 10 + (ord(sa[i]) - ord('0'))) % MOD
  
     # calculating b % (MOD - 1)
-    for i in range(len(sb)):
-        b = (b * 10 + (ord(sb[i]) -
-                       ord('0'))) % (MOD - 1);
+    for i in range(len(sb)): #Shuffles through each digit, multipling by 10, adding next digit, taking modulus
+        b = (b * 10 + (ord(sb[i]) - ord('0'))) % (MOD - 1)
     return powerLL(a, b);
 
 sum = 0
 temp = 0
 for i in range(1,1001): #from 1 to 1000
     sa = sb = "{}".format(i) #convert int to string
-    #print(i, powerStrings(sa, sb));
+    #print(i, powerStrings(sa, sb))
     sum = int(powerStrings(sa, sb)) + sum #convert string to int and sum
 
 print(sum % MOD) #Only want last 10 digits
-# This code is contributed by mits
